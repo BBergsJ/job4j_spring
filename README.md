d61ab06c4415546c81d2362e486d47ec6c36fb71
BBergsJ <goldlike888@gmail.com>
2022-01-05 20:09:23 +0300

    Что такое DI? [#6876]

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # job4j_spring
+
+Уроки и задания по Spring Framework
---
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -0,0 +1,26 @@
+package ru.job4j.di;
+
+public class StartUI {
+
+    private Store store;
+    private ConsoleInput consoleInput;
+
+    public StartUI(Store store) {
+        this.store = store;
+        this.consoleInput = new ConsoleInput();
+    }
+
+    public void add(String value) {
+        store.add(value);
+    }
+
+    public String ask(String question) {
+        return consoleInput.askStr(question);
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+}
